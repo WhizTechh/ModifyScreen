8f89440049296c4f597525edbe6f7f3d1ce35039
WhizTech <153662005+WhizTechh@users.noreply.github.com>
2024-08-23 19:12:16 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,40 @@
+﻿//By WhizTech
+
+using static Microsoft.Maui.Devices.DeviceDisplay;
+
+namespace ModifyScreen
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            var screenWidth = MainDisplayInfo.Width / MainDisplayInfo.Density;
+            var screenHeight = MainDisplayInfo.Height / MainDisplayInfo.Density;
+
+            var windowWidth = 800;
+            var windowHeight = 500;
+
+            //center window
+            window.X = (screenWidth - windowWidth) / 2;
+            window.Y = (screenHeight - windowHeight) / 2;
+
+            //Fix window size
+            window.MinimumWidth = windowWidth;
+            window.MinimumHeight = windowHeight;
+
+            window.MaximumWidth = windowWidth;
+            window.MaximumHeight = windowHeight;
+
+            return window;
+        }
+    }
+}
